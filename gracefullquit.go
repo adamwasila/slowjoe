22b22395cec1526f7f144e499a3eeb98b6a1c17f
Adam Wasila <adam.wasila@gmail.com>
2020-02-02 12:39:12 +0100

    Add more tracing logs to graceful quit safeguards

--- gracefullquit.go
+++ gracefullquit.go
@@ -28,16 +28,20 @@ type safeBlock struct{}
 // SafeBlock marks start of code block that should be guarded
 func SafeBlock() safeBlock {
 	safeGroup.Add(1)
+	logrus.Trace("Entering safeguarded block")
 	return safeBlock{}
 }
 
 // StopBlocking marks end of code block that should be guarded
 func StopBlocking(safeBlock) {
+	logrus.Trace("Leaving safeguarded block")
 	safeGroup.Done()
 }
 
 // SafeQuit won't return if there is any guarded block of code
 // that is still executed.
 func SafeQuit() {
+	logrus.Trace("Waiting for all safeguarded blocks to be leaved")
 	safeGroup.Wait()
+	logrus.Trace("It's now safe to turn off your application")
 }
